60b3cd0c52400ee6ae612392ab17d79c3e50261a
hirokuni-k <shisenramen@yahoo.co.jp>
2021-04-28 23:34:42 +0900

    旅行プログラム（メソッド）

---
+++ main.rb
@@ -0,0 +1,11 @@
+require "./methods.rb"
+plans = [
+  { place: "沖縄", price: 10000 },
+  { place: "北海道", price: 20000 },
+  { place: "九州", price: 15000 },
+]
+
+disp(plans)
+chosen_plan = choose(plans)
+number_of_people = decide_number(chosen_plan)
+caluculate_charges(chosen_plan, number_of_people)
---
+++ methods.rb
@@ -0,0 +1,38 @@
+def disp(plans)
+  puts "旅行プランを選択して下さい。"
+  plans.each.with_index(1) do |plan, i|
+    puts "#{i}.#{plan[:place]} (#{plan[:price]}円)"
+  end
+end
+
+def choose(plans)
+  while true
+    print "プランの番号を選択>"
+    select_plans_num = gets.to_i
+    break if (1..3).include?(select_plans_num)
+    puts "1~3の番号を入力して下さい"
+  end
+  plans[select_plans_num - 1]
+end
+
+def decide_number(chosen_plan)
+  puts "#{chosen_plan[:name]}ですね。"
+  puts "何名で予約されますか？"
+  while true
+    print "人数を入力>"
+    number_of_people = gets.to_i
+    break if number_of_people >= 1
+    puts "1名以上を入力して下さい"
+  end
+  number_of_people
+end
+
+def caluculate_charges(chosen_plan, number_of_people)
+  puts "#{number_of_people}名ですね。"
+  total_price = chosen_plan[:price] * number_of_people
+  if number_of_people >= 5
+    puts "5名以上なので10%割引となります"
+    total_price *= 0.9
+  end
+  puts "合計金額は#{total_price.floor}円になります。"
+end
